04e12eaed33468c9d7be0249ef76dd7a6828441f
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-04-06 09:19:28 -0500

    Fix Mojang Skill Issue: * Interface?

--- src/main/java/io/github/vampirestudios/craftmineplus/pond/CustomIconsDuck.java
+++ src/main/java/io/github/vampirestudios/craftmineplus/pond/CustomIconsDuck.java
@@ -3,7 +3,7 @@ package io.github.vampirestudios.craftmineplus.pond;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.mines.CustomIcons;
 
-public class CustomIconsDuck {
+public interface CustomIconsDuck {
     static ResourceLocation register(ResourceLocation resourceLocation) {
         CustomIcons.ICONS.add(resourceLocation);
         return resourceLocation;
