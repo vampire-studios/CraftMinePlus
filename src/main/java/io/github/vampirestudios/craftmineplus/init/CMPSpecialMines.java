e53f492c98467d25c1125e51f0b98eb6534e5b40
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-04-07 00:49:22 -0500

    Fix cputnam-a11y Skill Issue: * Make SpecialMine namespaced

--- src/main/java/io/github/vampirestudios/craftmineplus/init/CMPSpecialMines.java
+++ src/main/java/io/github/vampirestudios/craftmineplus/init/CMPSpecialMines.java
@@ -1,21 +1,23 @@
 package io.github.vampirestudios.craftmineplus.init;
 
 import io.github.vampirestudios.craftmineplus.CraftminePlus;
+import io.github.vampirestudios.craftmineplus.pond.SpecialMineDuck;
 import net.minecraft.world.level.UnlockCondition;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.mines.SpecialMine;
 import net.minecraft.world.level.mines.WorldEffects;
 
 public class CMPSpecialMines {
-	public static final SpecialMine MAGMA_BOSS = SpecialMine.builder(CraftminePlus.id("magma_boss").toString())
-			.withRequiredEffects(
-					CMPWorldEffects.NETHER_WORLD, CMPWorldEffects.MAGMA_BOSS_FIGHT, WorldEffects.EVENT_EXIT
-			)
-			.withOneOf(WorldEffects.BIOMES)
-			.addUnlockedBy(UnlockCondition.blockBreak((serverLevel, blockState) ->
-					blockState.is(Blocks.MAGMA_BLOCK) || blockState.is(Blocks.NETHERRACK)
-			))
-			.register();
+    public static final SpecialMine MAGMA_BOSS = SpecialMineDuck.namespacedBuilder(CraftminePlus.id("magma_boss"))
+            .withRequiredEffects(
+                    CMPWorldEffects.NETHER_WORLD, CMPWorldEffects.MAGMA_BOSS_FIGHT, WorldEffects.EVENT_EXIT
+            )
+            .withOneOf(WorldEffects.BIOMES)
+            .addUnlockedBy(UnlockCondition.blockBreak((serverLevel, blockState) ->
+                    blockState.is(Blocks.MAGMA_BLOCK) || blockState.is(Blocks.NETHERRACK)
+            ))
+            .register();
 
-	public static void init() {}
+    public static void init() {
+    }
 }
